33b51c9fd99b9d7a031d2b3021b8938c4a7bf8c3
worifung achu <achuclinton47@gmail.com>
2025-12-15 13:35:20 -0700

    feat:created admin navbar

---
+++ app/(administrator)/admin/additem/page.tsx
@@ -0,0 +1,8 @@
+export default function AddItemPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Add New Item</h1>
+      {/* Add your form or content for adding a new item here */}
+    </div>
+  );
+}
---
+++ components/admin-nav.tsx
@@ -0,0 +1,93 @@
+"use client";
+import { useState } from "react";
+import Link from "next/link";
+import { Menu, X } from "lucide-react";
+
+export function AdminNavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="w-full flex absolute top-0 py-3 items-center justify-between px-6 bg-white shadow-md rounded-xl flex-col sm:flex-row gap-4">
+      <span className="text-lg font-light flex items-center text-black w-full justify-between sm:w-fit">
+        <h1>MoodChangingCafe | Admin</h1>
+        <div
+          className="sm:hidden cursor-pointer hover:bg-accent p-2 rounded-full"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          {isOpen ? <X /> : <Menu />}
+        </div>
+      </span>
+      <span className="hidden sm:block">
+        <ul className=" gap-4 [&>li]:list-none hidden sm:flex">
+          <li>
+            <Link href="/admin" className="hover:underline underline-offset-2 ">
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/admin/products"
+              className="hover:underline underline-offset-2 "
+            >
+              Products
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/admin/orders"
+              className="hover:underline underline-offset-2 "
+            >
+              Orders
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/admin/users"
+              className="hover:underline underline-offset-2 "
+            >
+              Users
+            </Link>
+          </li>
+        </ul>
+      </span>
+      <div className={`sm:hidden w-full ${isOpen ? "block" : "h-0 hidden "} `} >
+          {isOpen && (
+            <ul className=" gap-4 [&>li]:list-none flex flex-col">
+              <li>
+                <Link
+                  href="/admin"
+                  className="hover:underline underline-offset-2 "
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/admin/products"
+                  className="hover:underline underline-offset-2 "
+                >
+                  Products
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/admin/orders"
+                  className="hover:underline underline-offset-2 "
+                >
+                  Orders
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/admin/users"
+                  className="hover:underline underline-offset-2 "
+                >
+                  Users
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      <span className="hidden md:block"></span>
+    </div>
+  );
+}
