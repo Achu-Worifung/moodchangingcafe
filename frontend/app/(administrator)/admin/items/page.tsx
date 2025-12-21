9e5e42a1805d05aa229a586a5897054f9516f053
Achu Worifung <achuworifung@gmail.com>
2025-12-21 01:02:43 -0700

    feat: display items with stock greater than 0

--- frontend/app/(administrator)/admin/items/page.tsx
+++ frontend/app/(administrator)/admin/items/page.tsx
@@ -3,32 +3,32 @@ import { useState, useEffect } from "react";
 import { ItemForm } from "@/components/ui/item-form";
 import { ItemFormProps } from "@/lib/types";
 import { toast } from "sonner";
-import {ItemTable} from "@/components/ui/item-table";
+import { ItemTable } from "@/components/ui/item-table";
 import Loading from "@/app/loading";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "@/lib/firebase";
 export default function EditItemPage() {
   const [items, setItems] = useState<{ id: string; [key: string]: any }[]>([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      async function fetchItems() {
-        try {
-          const snapshot = await getDocs(collection(db, "items"));
-          const items = snapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-          }));
-          setItems(items);
-          console.log("Fetched items:", items);
-        } catch (error) {
-          console.error("Error fetching items:", error);
-        } finally {
-          setLoading(false);
-        }
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const snapshot = await getDocs(collection(db, "items"));
+        const items = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(items);
+        console.log("Fetched items:", items);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      } finally {
+        setLoading(false);
       }
-      fetchItems();
-    }, []);
+    }
+    fetchItems();
+  }, []);
   if (loading) {
     return (
       <div>
@@ -44,9 +44,9 @@ export default function EditItemPage() {
         <div>No items found.</div>
       ) : (
         <div className="space-y-8 flex flex-col items-center justify-start p-8 w-full min-h-screen mt-20">
-            <h1 className="text-4xl font-bold text-center">View Items</h1>
+          <h1 className="text-4xl font-bold text-center">View Items</h1>
 
-            <ItemTable data={items} />
+          <ItemTable data={items} />
         </div>
       )}
     </>
--- frontend/app/(main)/page.tsx
+++ frontend/app/(main)/page.tsx
@@ -2,34 +2,48 @@
 import Image from "next/image";
 import { toast } from "sonner";
 import {useState, useEffect} from "react";
-import {ItemFormProps} from "../lib/types";
 import { useRouter } from 'next/navigation';
+import { collection } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+import Loading from "@/app/loading";
+import { query, where, getDocs } from "firebase/firestore";
+
 export default function Home() {
-  const [items, setItems] = useState<ItemFormProps[] | null>([]);
   const router = useRouter();
 
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("http://127.0.0.1:8000/api/items");
-      if (!res.ok) {
-        toast.error("Failed to fetch items. Please try again.");
-        return;
-      }
-      const data = await res.json();
-      setItems(data.items);
-      console.log("Fetched items:", data.items);
-    }
-    fetchData();
-  }, []);
+  const [items, setItems] = useState<{ id: string; [key: string]: any }[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  function hexToBase64(hex) {
-  const bytes = new Uint8Array(
-    hex.match(/.{1,2}/g).map(byte => parseInt(byte, 16))
-  );
-  let binary = "";
-  bytes.forEach(b => binary += String.fromCharCode(b));
-  return btoa(binary);
-}
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+         const q = query(
+          collection(db, "items"),
+          where("stock", ">", 0)
+        );
+        const snapshot = await getDocs(q);
+        const items = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(items);
+        console.log("Fetched items:", items);
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchItems();
+  }, []);
+  if (loading) {
+    return (
+      <div className="text-center">
+        <Loading />
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <div className="container mx-auto p-4">
@@ -39,13 +53,13 @@ export default function Home() {
           <div key={item.id} className="border rounded-lg p-4 shadow-md cursor-pointer hover:shadow-lg transition-shadow ease-in-out"
           onClick={() => router.push(`/item/${item.id}`)} >
             <img
-              src={ item.img && `data:image/png;base64,${hexToBase64(item.img)}`}
+              src={ item.img}
               alt={item.name || "Item image"}
               className="w-full h-48 object-cover rounded-md mb-4"
             />
             <h2 className="text-lg font-semibold">{item.name}</h2>
             {/* <p className="text-sm text-gray-600">{item.description}</p> */}
-            <p className="text-sm font-medium mt-2">Price: ${item.unit_price.toFixed(2)}</p>
+            <p className="text-sm font-medium mt-2">Price: ${item.unit_price}</p>
             {/* <p className="text-sm">Stock: {item.quantity_in_stock}</p> */}
             {/* <p className="text-sm">Tax Rate: {item.tax_rate}%</p> */}
             <p className="text-sm">Category: {item.category}</p>
